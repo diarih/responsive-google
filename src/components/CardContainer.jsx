423d051f3ff8fe2e79bf1ca7552caa3e0fb6a79f
Diar Ihza <diarihza@gmail.com>
2024-09-11 10:38:14 +0700

    fix: fix aja lah

--- src/components/CardContainer.jsx
+++ src/components/CardContainer.jsx
@@ -1,11 +1,39 @@
 import React from 'react';
-import {Card, CardContent} from "@/components/ui/card.jsx";
+import {Card} from "@/components/ui/card.jsx";
+import {Input} from "@/components/ui/input.jsx";
+import {Button} from "@/components/ui/button.jsx";
 
-const CardContainer = ({children}) => {
+const CardContainer = () => {
   return (
     <Card
       className={'p-6 sm:p-9 shadow-none border-0 bg-white w-full lg:w-7/12 flex justify-between flex-col sm:flex-row gap-y-8'}>
-      {children}
+      <div className={'flex-1 space-y-6'}>
+        <h1 className={'font-normal text-5xl'}>Login</h1>
+        <p>Gunakan akun google anda</p>
+      </div>
+      <div className={'flex-1'}>
+        <div className={'flex flex-col gap-8 sm:gap-6'}>
+          <div>
+            <Input className={'py-6'} type="email" placeholder="Email atau nomor telepon"/>
+            <div className={'pt-1'}>
+              <Button className={'bg-transparent border-0 px-0 font-medium'} variant={'link'} asChild>
+                <a href="/login">Lupa email?</a>
+              </Button>
+            </div>
+          </div>
+          <div>
+            <div className={'text-sm leading-tight'}>Bukan komputer Anda? Gunakan Private Window untuk login.
+              <Button className={'bg-transparent border-0 px-0 font-medium text-wrap'} variant={'link'} asChild>
+                <a href="/login">Pelajari lebih lanjut cara menggunakan mode tamu</a>
+              </Button>
+            </div>
+          </div>
+          <div className={'flex justify-between sm:justify-end gap-4'}>
+            <Button variant={'secondary'}>Buat akun</Button>
+            <Button>Selanjutnya</Button>
+          </div>
+        </div>
+      </div>
     </Card>
   );
 };
--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 
 const Container = ({children}) => {
   return (
-    <div className={'h-screen w-screen flex flex-col items-center justify-center py-0 lg:py-24 bg-white lg:bg-[#F0F4F9]'}>
+    <div
+      className={'h-screen w-screen flex flex-col items-center justify-center py-0 lg:py-24 bg-white lg:bg-[#F0F4F9]'}>
       {children}
     </div>
   );
